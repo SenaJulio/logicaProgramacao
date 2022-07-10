6a7bc41114a1c9dcd17a992096597d6940ead986
JULIO SENA <gm.sena@hotmail.com>
2022-07-09 22:10:06 -0300

    iniciando na logica

---
+++ desafio_aritmeticos.js
@@ -0,0 +1,29 @@
+// valares literais
+console.log(7 + 3);
+console.log(6 - 7);
+console.log(7 * 9);
+console.log(8 / 3);
+console.log(8 % 3);
+
+let a = 8;
+let b = 11;
+
+
+//usando variaveis
+console.log( a + b);
+console.log(a - b);
+console.log( a * b);
+console.log(a / b);
+console.log(a % b);
+
+a = a + b * a / b;
+console.log(a);
+
+
+//desafio
+
+let f = 73;
+
+// f - 32 / 9 = c / 5
+let c = 5 *(73 - 32) / 9
+console.log(c);
---
+++ operadoresrelacionias.js
@@ -0,0 +1,29 @@
+7 > 3
+8 >= 9
+3 < 4
+7 <= 10
+7 == 7
+8 != 9
+
+//verdadeiro ou falso
+  // True      ||  false
+
+   // operadores relacionais terá resposta verdadeiro ou falso.
+
+   // valores literais
+   console.log(7 > 9);
+   console.log(7 >= 7);
+   console.log(8 < 2);
+   console.log(8 <= 12);
+   console.log(9 == 3);
+   console.log(9 != 5);
+
+   let a = 7;
+   let b = 32;
+
+   console.log(a > b);
+   console.log(a >= b);
+   console.log(a < b);
+   console.log(a <= b);
+   console.log(a == b);
+   console.log(a != b);
